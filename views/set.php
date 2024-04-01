85fe76e3a59eb790786777e9f901fed571899e6b
Melvin <melvinhqb@cy-tech.fr>
2024-04-01 15:30:41 +0200

    Edit set.php

--- views/set.php
+++ views/set.php
@@ -5,18 +5,19 @@
     <h1>Extension <?php echo $set->name; ?></h1>
     <?php if (!empty($cards)): ?>
         <div>
+            <!-- Ajouter bouton filtre qui exécute un JS qui filtre en fonnction de la colonne rareté, prix et type -->
             <table border='1'>
                 <tr>
                     <th>Image</th>
                     <th>Nom</th>
-                    <th>LOCAL ID</th>
+                    <th>Rareté</th>
                     <th>Prix</th>
                 </tr>
                 <?php foreach ($cards as $card): ?>
                     <tr>
                         <td><img class='card_image' src='<?php echo $card->image; ?>/low.png' alt='<?php echo $card->name; ?>'></td>
                         <td><a class='card-link' href='index.php?route=products&card=<?php echo $card->id; ?>'><?php echo $card->name; ?></a></td>
-                        <td><?php echo $card->localId; ?></td>
+                        <td><?php echo $card->rarity; ?></td>
                         <td><?php echo number_format($card->price, 2, ',', '.'); ?> €</td>
                     </tr>
                 <?php endforeach; ?>
