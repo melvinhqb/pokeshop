c8fcad3a3238a607c67cb432fec0bc0f6bdc415d
Rim086 <dragonlair1404@gmail.com>
2024-04-02 10:46:52 +0200

    police

--- views/layout.php
+++ views/layout.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokeshop | Accueil</title>
     <link rel="stylesheet" href="ressources/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
