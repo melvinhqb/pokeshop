ff475c6cf0b4256868d8fccc7f8a57f771f16849
Melvin <melvinhqb@cy-tech.fr>
2024-04-10 00:03:12 +0200

    ras

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require_once('app/controllers/SerieController.php');
 require_once('app/controllers/CardController.php');
 require_once('app/controllers/SetController.php');
 require_once('app/controllers/UserController.php');
-require_once('app/controllers/NotfoundController.php');
 
 // Utilisation des espaces de noms pour simplifier les références
 use App\Controllers\Controller;
@@ -16,7 +15,6 @@ use App\Controllers\SerieController;
 use App\Controllers\SetController;
 use App\Controllers\CardController;
 use App\Controllers\UserController;
-use App\Controllers\NotfoundController;
 
 // Fonction principale de routage
 function route($route) {
