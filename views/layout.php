9a5f8aafc3bb3a5924a2a9771daea685984a0d82
Sara <saraguendouz222@gmail.com>
2024-04-11 16:10:06 +0200

    ajout verificationPHP.php

--- views/layout.php
+++ views/layout.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="verificationJS.js" defer></script>
+    
 </head>
 <body>
     <?php require('partials/header.php'); ?>
